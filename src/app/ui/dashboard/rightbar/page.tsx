ad045c572585564c9bbd5d6bd172c589a39d6345
Muhammadali7866 <malikali8157@gmail.com>
2024-11-18 18:22:08 +0500

    right bar flow done

--- src/app/ui/dashboard/rightbar/page.tsx
+++ src/app/ui/dashboard/rightbar/page.tsx
@@ -14,7 +14,9 @@ const RightBar = () => {
           <h3 className={styles.title}>
             How to use the new version of the admin dashbaord
           </h3>
-          <p className={styles.title}>
+          <span className={styles.subtitles}>Take 4 minutes to Learn</span>
+
+          <p className={styles.desc}>
             TO use the admin dashbaord you have the deligated access to the new
             version of the build then you can easily access it
           </p>
@@ -26,21 +28,16 @@ const RightBar = () => {
       </div>
       <div className={styles.item}>
         <div className={styles.bgContainer}>
-          <Image
-            src="/file.svg"
-            alt=""
-            width={40}
-            height={40}
-            className={styles.userImage}
-          />{" "}
+          <Image src="/file.svg" alt="" className={styles.bg} fill />{" "}
         </div>
         <div className={styles.texts}>
           <span className={styles.notifications}>Available Now</span>
           <h3 className={styles.title}>
             How to use the new version of the admin dashbaord
           </h3>
-          <p className={styles.title}>
-            TO use the admin dashbaord you have the deligated access to the new
+          <span className={styles.subtitles}>Take 4 minutes to Learn</span>
+          <p className={styles.desc}>
+            To use the admin dashbaord you have the deligated access to the new
             version of the build then you can easily access it
           </p>
           <button className={styles.button}>
